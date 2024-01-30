6b6691faaa1284fd4202db4cb50baee4f2040bd7
Vishva Prakash <vishvaprakashmaddheshiya@gmail.com>
2024-01-30 17:15:22 +0530

    shopCategory, loginsignup

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import Breadcrum from '../Components/Breadcrums/Breadcrum';
 
 const Product = () => {
+  const {all_product} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_product.find((e)=> e.id === Number(productId))
   return (
-    <div>
-      
+    <div className=''>
+      <Breadcrum product={product}/>
     </div>
   )
 }
